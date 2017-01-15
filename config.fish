ec94487af1518c22695c9a4a6f313a43f5c4584a
benson40111 <benson40111@gmail.com>
2017-01-15 17:32:49 +0800

    first upload some setting

---
+++ config.fish
@@ -0,0 +1,15 @@
+export TERM=screen-256color
+if [ (tmux ls | grep -c "window") = "1"]; and [ (echo $TMUX) = "" ]
+	tmux attach-session -d
+else
+	tmux
+end
+clear
+screenfetch
+function fish_prompt
+	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/prompt.sh left
+end 
+
+function fish_right_prompt
+	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/prompt.sh right
+end
---
+++ run.sh
@@ -0,0 +1,6 @@
+echo "auto copy to the folder..."
+cp .tmux.conf ~/.tmux.conf
+cp config.fish ~/.config/fish/config.fish
+cp prompt.sh ~/
+echo "end!"
+
