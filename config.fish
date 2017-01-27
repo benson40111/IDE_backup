f19624c723eaee424674bcab92af450bae768393
benson40111 <benson40111@gmail.com>
2017-01-27 16:59:50 +0800

    let prompt.sh be hiden file

--- config.fish
+++ config.fish
@@ -7,9 +7,9 @@ end
 clear
 screenfetch
 function fish_prompt
-	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/prompt.sh left
+	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.prompt.sh left
 end 
 
 function fish_right_prompt
-	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/prompt.sh right
+	env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.prompt.sh right
 end
